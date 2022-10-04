3b37e1a24681537e4c7c490d9cbf9db6d6c5c177
Parkwochang <whwh3560@gmail.com>
2022-10-04 23:38:40 +0900

    token

--- src/hooks/useToken.ts
+++ src/hooks/useToken.ts
@@ -2,6 +2,7 @@ import { useSelector } from 'react-redux';
 
 import { RootState } from '../redux/modules/rootReducer';
 
+//token
 export default function useToken() {
   const token = useSelector<RootState, string | null>(
     (state) => state.auth.token,
